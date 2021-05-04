1e89bcf3b05c5c00d46189c92247c4792d207f5e
Oliver Udvardi <oudvardi@gmail.com>
2021-05-04 21:03:30 +0200

    Initial commit

---
+++ src/regexmatcher/RegexMatcher.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regexmatcher;
+
+/**
+ *
+ * @author DADO
+ */
+public class RegexMatcher {
+
+    /**
+     * @param args the command line arguments
+     */
+
+}
